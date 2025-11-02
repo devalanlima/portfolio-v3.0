07aac6fcef4a11e9effda7e4a368759af56fba0d
AlanLima <dev.alanlima@gmail.com>
2025-11-02 17:31:20 -0300

    feat: create alan lima logo component

---
+++ src/ui/alanLima.tsx
@@ -0,0 +1,7 @@
+export default function AlanLima() {
+  return (
+    <h1 className="font-bayer-universal block h-12 -translate-y-1 text-7xl [word-spacing:-15px]">
+      alan lima
+    </h1>
+  );
+}
