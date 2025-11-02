f3f2fda81a127cb8a3b96bc0fa28186fe6e3d995
AlanLima <dev.alanlima@gmail.com>
2025-11-02 13:05:31 -0300

    feat: create new main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "../styles/global.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import MainLayout from "@/features/layout/MainLayout";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,13 +12,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+  return <MainLayout>{children}</MainLayout>;
 }
---
+++ src/features/layout/MainLayout.tsx
@@ -0,0 +1,49 @@
+import { Geist, Geist_Mono, Bricolage_Grotesque } from "next/font/google";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { cn } from "@/lib/utils";
+import localFont from "next/font/local";
+
+const bayerUniversal = localFont({
+  variable: "--font-bayer-universal",
+  src: "../../app/fonts/Bayer-Universal.woff2",
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const bricolageGrotesque = Bricolage_Grotesque({
+  variable: "--font-bricolage-grotesque",
+  subsets: ["latin"],
+});
+
+export default function MainLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={cn(
+          "antialiased min-h-screen flex flex-col",
+          geistSans.variable,
+          geistMono.variable,
+          bricolageGrotesque.variable,
+          bayerUniversal.variable
+        )}
+      >
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
